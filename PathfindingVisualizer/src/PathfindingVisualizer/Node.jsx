3d909ad89e20b00b9e80982df36abacb7069313a
ant1010 <arreola.antony@gmail.com>
2020-01-31 16:06:41 -0700

    update to grid
    Picking start and goal nodes aswell as selecting wall nodes now possible.

--- PathfindingVisualizer/src/PathfindingVisualizer/Node.jsx
+++ PathfindingVisualizer/src/PathfindingVisualizer/Node.jsx
@@ -7,10 +7,65 @@ export default class node extends Component {
         this.state = {
            col:0,
            row:0,
+           isWat:'node',
+           isStart: false,
+           isGoal: false,
            
         };
+        this.handleClick = this.handleClick.bind(this)
+        this.handleDoubleClick = this.handleDoubleClick.bind(this)
+    }
+    handleClick(e){
+        e.preventDefault();
+        const status = this.state.isWat;
+        console.log("will it null " + this.props.startNode)
+        if(status === 'node'){
+
+        if(this.props.startNode !== null && this.props.goalNode !== null ){
+            this.setState({isWat:'wallNode'})
+            this.props.incrementCount('node',this.props.row,this.props.col);
+            return;
+        }
+       else if( (this.props.goalNode === null && this.props.startNode === null) || (this.props.goalNode !== null && this.props.startNode === null)){
+           this.setState({isWat:'startNode'});
+           this.props.incrementCount('startNode',this.props.row,this.props.col);
+           return;
+        }
+        else if(this.props.goalNode === null && this.props.startnode !== null){
+            this.setState({isWat:'goalNode'});
+            this.props.incrementCount('goalNode',this.props.row,this.props.col);
+            return;
+        }
+        
+       
+    }
+    if(status !== 'node'){
+        this.props.decrementCount(this.state.isWat,0,0);
+        this.setState({isWat:'node'});
+        return;
+    }
+    }
+        handleDoubleClick(e){
+            e.preventDefault();
+            const status = this.state.isWat;
+            this.setState({isWat:"hello"});
+           
+            if(status === 'node'){
+               this.setState({isWat:'node-goal'});
+               this.props.incrementCount();
+            }
+            if(status !== 'node'){
+                this.setState({isWat:'node'});
+                this.props.decrementCount();
+               
+            }
+    
     }
     render(){
-        return <div className="node"></div>
+        const test = this.props.row;
+        
+        
+        
+        return <div className={`node ${this.state.isWat}`} onDoubleClick={this.handleDoubleClick} onClick = {this.handleClick}></div>;
     }
 }
--- PathfindingVisualizer/src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ PathfindingVisualizer/src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -3,38 +3,102 @@ import Node from './Node'
 
 import './PathfindingVisualizer.css';
 
+const INITIAL_START_COL = 1;
+const INITIAL_START_ROW = 10;
+const INITIAL_GOAL_COL = 19;
+const INITIAL_GOAL_ROW = 10;
+
 export default class PathfindingVisualizer extends Component {
     constructor(props){
         super(props);
         this.state = {
-            nodes: [],
+            grid: [],
+            coloredNodeCount: 0,
+            goalNode: null,
+            startNode: null
         };
-        
+        this.handlenodeClick = this.handlenodeClick.bind(this);
+        this.incrementColoredNodeCount = this.incrementColoredNodeCount.bind(this);
+        this.decrementColoredNodeCount = this.decrementColoredNodeCount.bind(this);
+        this.updateGoalStartNode = this.updateGoalStartNode.bind(this);
     }
     componentDidMount(){
+        const grid = getInitialGrid();
+        this.setState({grid})
         const nodes = []
-        for(let row = 0; row < 15; row++){
-            const currentRow = [];
-            for(let col = 0; col < 50;++col){
-                currentRow.push([]);
-            }
-            nodes.push(currentRow);
-        }
-        this.setState({nodes})
     }
+    handlenodeClick(e)
+    {
+        e.preventDefault()
+        
 
+    }
+    incrementColoredNodeCount(nodeType,row,col){
+         let count = this.state.coloredNodeCount+1;
+         this.setState({coloredNodeCount:count});
+         
+        if(nodeType !== 'node'){
+            const location = {row,col}
+            this.setState({[nodeType]:location});
+        }  
+    }
+    decrementColoredNodeCount(nodeType,col,row){
+        console.log(this.state.startNode)
+        if(nodeType !== 'node'){
+            this.setState({[nodeType]:null});
+            
+        }
+        let count = this.state.coloredNodeCount - 1;
+        this.setState({coloredNodeCount:count});
+        
+    }
+    updateGoalStartNode(nodeType, newStatus){
+        this.setState({[nodeType]:newStatus});
+    }
     render(){
-        const {nodes} = this.state;
-        console.log(nodes)
+        const {grid} = this.state;
+        console.log(grid)
+        const isStart = false;
+        const isGoal = false;
 
         return(
             <div className ="grid">
-              {nodes.map((row,rowIdx) => {
-                  return <div>
-              {row.map((node,nodeIdx) => <Node></Node>)}
+              {grid.map((row,rowIdx) => {
+                  return <div className = "grid-row" >
+              {row.map((node,nodeIdx) => 
+              <Node 
+              decrementCount = {this.decrementColoredNodeCount.bind(this)} 
+              incrementCount = {this.incrementColoredNodeCount.bind(this)} 
+              nodeCount = {this.state.coloredNodeCount}
+              row = {node.row} 
+              col = {node.col} 
+              goalNode = {this.state.goalNode}
+              startNode = {this.state.startNode}
+              
+              >
+              </Node>)}
               </div>
               })}
             </div>
         );
     }
 }
+const getInitialGrid = () => {
+    const grid = []
+    for(let row = 0; row < 20; row++){
+        const currentRow = [];
+        for(let col = 0; col < 20;++col){
+            currentRow.push(createNode(col, row));
+        }
+        grid.push(currentRow);
+    }
+    return grid;
+};
+const createNode = (col,row) => {
+    return {
+        row,
+        col,
+        isStart: col === INITIAL_START_COL && row === INITIAL_START_ROW,
+        isGoal: col === INITIAL_GOAL_COL && row === INITIAL_GOAL_ROW
+    };
+};
